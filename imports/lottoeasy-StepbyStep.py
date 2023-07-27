0a5af09612bda1b1e39b78e39751ceb09f60803c
cesarac666 <38736321+cesarac666@users.noreply.github.com>
2023-07-27 18:25:46 -0300

    Arquivo de classes

---
+++ imports/lottoeasy-StepbyStep.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class LoadData:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.dataframe = None
+
+    def read_csv(self):
+        self.dataframe = pd.read_csv(self.filepath, sep=',')
+
+
+
+        
+        
+ 
